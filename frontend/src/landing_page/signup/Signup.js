6e067f9b73dfc7aeeec4b20b23001d27765ebb03
AtharvA89 <106697005+AtharvA89@users.noreply.github.com>
2025-01-07 18:54:34 +0530

    Update Signup.js

--- frontend/src/landing_page/signup/Signup.js
+++ frontend/src/landing_page/signup/Signup.js
@@ -36,7 +36,7 @@ const Signup = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:3002/signup",
+        "https://project-zerodha-gh5c.onrender.com/signup",
         {
           ...inputValue,
         },
